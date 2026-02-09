44690cc19bd8736932bf8d5e8564ce440f203acf
Wendymackennah <wendymackennah5@gmail.com>
2026-02-09 21:38:16 +0300

    footer

---
+++ elyssa-fruits/src/Components/Footer.jsx
@@ -0,0 +1,8 @@
+import { Box, Typography } from '@mui/material';
+export default function Footer() {
+return (
+<Box bgcolor="#1F2937" color="#FFF" py={6} textAlign="center">
+<Typography variant="body2">© 2026 ELYSSA Fruits. All rights reserved.</Typography>
+</Box>
+);
+}
