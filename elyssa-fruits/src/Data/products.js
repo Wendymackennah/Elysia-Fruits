f09e09d4028facc27fe3f64ae4b2f96767305a7c
Wendymackennah <wendymackennah5@gmail.com>
2026-02-09 21:49:55 +0300

    Products

---
+++ elyssa-fruits/src/Data/products.js
@@ -0,0 +1,12 @@
+export const categories = [
+  { name: 'Fresh Fruits', emoji: '🍎' },
+  { name: 'Juices', emoji: '🥤' },
+  { name: 'Dried Fruits', emoji: '🍌' },
+  { name: 'Hampers', emoji: '🎁' },
+];
+
+export const products = [
+  { name: 'Mango Box', price: 1200, image: 'https://images.unsplash.com/photo-1506806732259-39c2d0268443' },
+  { name: 'Berry Mix', price: 1500, image: 'https://images.unsplash.com/photo-1464965911861-746a04b4bca6' },
+  { name: 'Green Detox Juice', price: 800, image: 'https://images.unsplash.com/photo-1514995669114-6081e934b693' },
+];
