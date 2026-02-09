c781b7241ff433ec05b59cf4b75c2f31617f8ce3
Wendymackennah <wendymackennah5@gmail.com>
2026-02-09 21:42:31 +0300

    main

--- elyssa-fruits/src/main.jsx
+++ elyssa-fruits/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { CartProvider } from './context/CartContext';
+import CssBaseline from '@mui/material/CssBaseline';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CssBaseline />
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </React.StrictMode>
+);
